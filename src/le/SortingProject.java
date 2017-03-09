c9f078e89ffca47694213b06badfa23e768c8915
958016796 <958016796@MLT210-22.acs.edcc.ctc.edu>
2017-03-08 17:30:34 -0800

    putting in different sorting methods

--- src/le/SortingProject.java
+++ src/le/SortingProject.java
@@ -11,6 +11,8 @@ import java.util.Random;
 public class SortingProject {
 
     /**
+     * Main method
+     *
      * @param args the command line arguments
      */
     public static void main(String[] args) {
@@ -19,37 +21,67 @@ public class SortingProject {
         for (int i = 0; i < arr.length; i++) {
             arr[i] = rand.nextInt(100);
         }
-        long startTime = System.nanoTime();  
+        long startTime = System.nanoTime();
+        //bubble sort
         bubbleSort(arr);
-        System.out.println("Time for Bubble Sort:    " 
+        System.out.println("Time for Bubble Sort:    "
                 + (System.nanoTime() - startTime));
         System.out.println("Bubble Sort:    " + Arrays.toString(arr));
+        //insection sort
         System.out.println("");
         insectionSort(arr);
-        System.out.println("Time for Insection Sort: " 
+        System.out.println("Time for Insection Sort: "
                 + (System.nanoTime() - startTime));
         System.out.println("Insection Sort: " + Arrays.toString(arr));
+        //selection sort
+        System.out.println("");
+        selectionSort(arr);
+        System.out.println("Time for Selection Sort: "
+                + (System.nanoTime() - startTime));
+        System.out.println("Selection Sort: " + Arrays.toString(arr));
+        //merge sort
+        System.out.println("");
+        mergeSort(arr);
+        System.out.println("Time for Merge Sort: "
+                + (System.nanoTime() - startTime));
+        System.out.println("Merge Sort:     " + Arrays.toString(arr));
+        //counting sort
+        System.out.println("");
+        countingSort(arr, 0, 99);
+        System.out.println("Time for Counting Sort: "
+                + (System.nanoTime() - startTime));
+        System.out.println("Counting Sort:  " + Arrays.toString(arr));
     }
-    
-    //insection sort
-    public static void insectionSort(int[] arr){
+
+    /**
+     * This is a insection sort, that has time complexity of O(n^2) and space
+     * complexity of O(1) with the average finish time: 6477401 nano-second
+     *
+     * @param arr the array to be sorted
+     */
+    public static void insectionSort(int[] arr) {
         int pos = 0;
         for (int i = 0; i < arr.length; i++) {
             pos = i;
-            while(pos > 0 && arr[pos - 1] > arr[pos]){
+            while (pos > 0 && arr[pos - 1] > arr[pos]) {
                 swap(arr, pos, pos - 1);
                 pos = pos - 1;
             }
         }
     }
-    
-    //bubble sort
-    public static void bubbleSort(int[] arr){
+
+    /**
+     * This is a insection sort, that has time complexity of O(n^2) and space
+     * complexity of O(1) with the average finish time: 5395589 nano-second
+     *
+     * @param arr the array to be sorted
+     */
+    public static void bubbleSort(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
             boolean swapped = false;
             for (int j = 1; j < arr.length - i; j++) {
                 if (arr[j] < arr[j - 1]) {
-                    swap(arr, j , j - 1);
+                    swap(arr, j, j - 1);
                     swapped = true;
                 }
             }
@@ -58,12 +90,102 @@ public class SortingProject {
             }
         }
     }
-    
-    //swapping method for the sort method
-    public static void swap(int[] arr, int i, int j){
+
+    /**
+     * This is a insection sort, that has time complexity of O(n^2) and space
+     * complexity of O(1) with the average finish time: 9993852 nano-second
+     *
+     * @param arr the array to be sorted
+     */
+    public static void selectionSort(int[] arr) {
+        int min = 0;
+        int max = arr.length - 1;
+        while (max > min) {
+            for (int i = min; i < max; i++) {
+                if (arr[i] < arr[min]) {
+                    swap(arr, i, min);
+                }
+                if (arr[i] > arr[max]) {
+                    swap(arr, i, max);
+                }
+            }
+            min = min + 1;
+            max = max - 1;
+        }
+    }
+
+    /**
+     * This is a merge sort, that has time complexity of O(n log n) and space
+     * complexity of O(n) with the average finish time: 11006066 nano-second
+     *
+     * @param arr the array to be sorted
+     */
+    public static void mergeSort(int[] arr) {
+        if (arr.length - 1 > 0) {
+            int[] arrLeft = new int[arr.length / 2];
+            System.arraycopy(arr, 0, arrLeft, 0, arr.length / 2);
+            int[] arrRight = new int[arr.length - arrLeft.length];
+            System.arraycopy(arr, arr.length / 2, arrRight, 0, arrRight.length);
+            mergeSort(arrLeft);
+            mergeSort(arrRight);
+            merge(arr, arrLeft, arrRight);
+        }
+    }
+
+    /**
+     * merging method for the merge sort
+     *
+     * @param arr the main array to be sorted
+     * @param arrLeft the array from the left
+     * @param arrRight the array from the right
+     */
+    public static void merge(int[] arr, int[] arrLeft, int[] arrRight) {
+        int i = 0;
+        int j = 0;
+        for (int k = 0; k < arr.length; k++) {
+            if (j > arrRight.length - 1 || (i <= arrLeft.length - 1
+                    && arrLeft[i] <= arrRight[j])) {
+                arr[k] = arrLeft[i];
+                i++;
+            } else {
+                arr[k] = arrRight[j];
+                j++;
+            }
+        }
+    }
+
+    /**
+     * This is a counting sort, that has time complexity of O(n + k) and space
+     * complexity of O(k) with the average finish time: 12981103 nano-second
+     *
+     * @param arr the array to be sorted
+     * @param max the max of the array
+     * @param min the min of the array
+     */
+    public static void countingSort(int[] arr, int min, int max) {
+        int[] temp = new int[max - min + 2];
+        for (int i = 0; i < arr.length; i++) {
+            temp[arr[i] - min]++;
+        }
+        int index = 0;
+        for (int i = 0; i <= max - min + 1; i++) {
+            for (int j = 0; j < temp[i]; j++) {
+                arr[index] = i + min;
+                index++;
+            }
+        }
+    }
+
+    /**
+     * the swapping method for all sorting method
+     *
+     * @param arr the array to be sorted
+     * @param i the first index
+     * @param j the second index
+     */
+    public static void swap(int[] arr, int i, int j) {
         int temp = arr[i];
         arr[i] = arr[j];
         arr[j] = temp;
     }
-    
 }
